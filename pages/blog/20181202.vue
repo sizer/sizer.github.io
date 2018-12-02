aca7058053a0139bf879ae58c92059ff789ae5ff
sizer <shinse.tanaka@gmail.com>
2018-12-02 19:45:07 +0900

    add contents 20181202

---
+++ pages/blog/20181202.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="container">
+    <div v-html="c20181202" />
+  </section>
+</template>
+
+<script>
+import c20181202 from '../../contents/20181202.md'
+
+export default {
+  components: {},
+  computed: {
+    c20181202() {
+      return c20181202
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  margin: 30px;
+}
+</style>
